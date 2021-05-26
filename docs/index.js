54d05e971ebbade720e48f71548589f7062269c0
Pedro Isac <ss.pedroisac@gmail.com>
2021-05-26 17:41:47 -0300

    Get starting page improvements

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,10 @@
 // === Copy text ===
-function copyText(id) {
-  console.log("copy");
+function copyText(index) {
+  const text = document.querySelectorAll(".code pre");
+  const range = document.createRange();
+  range.selectNodeContents(text[index]);
+  const sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+  document.execCommand("copy");
 }
